3f9d6f6e62988935557de583b0fc70000e8bb817
aarver <bacsajozseff@gmail.com>
2023-06-25 19:41:14 +0200

    Finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios'
+import {useEffect, useState} from 'react'
+import Coin from './components/Coin';
 
 function App() {
+
+  const [coinList, setCoinList] = useState([]);
+  const [search, setSearch ] = useState("");
+
+  useEffect(()=>{
+
+      Axios.get("https://api.coinstats.app/public/v1/coins")
+          .then(res => setCoinList(res.data.coins))
+
+  }, [])
+
+
+  const searchedCoin = coinList.filter(coin => {
+
+    //Megnézem, hogy a coinListben eltárolt összes kriptovaluta közül van-e egyezés a nevében azzal amit beírtam az input mezőbe
+    return coin.name.toLowerCase().includes(search.toLowerCase());
+  })
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="crypto-header">
+            <h1>Cryptovaluta Kereső</h1>
+
+            <input type="text" placeholder="Bitcoin..." onChange={(e)=> setSearch(e.target.value) }/>
+        </div>
+
+        <div className="crypto-container">
+            {
+              searchedCoin.map(c => {
+
+                return <Coin key={c.name} name={c.name} icon={c.icon} price={c.price} symbol={c.symbol}/>
+              })
+
+            }
+        </div>
     </div>
   );
 }
